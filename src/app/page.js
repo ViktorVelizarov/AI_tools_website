37a245ccff2528a887550f22b5f8a4624c8a8c14
ViktorVelizarov <VVVelizarov18@codingburgas.bg>
2024-05-22 22:22:22 +0200

    fix navigation links and add a loading animation for ToolCards

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,9 @@
-"use client"
+"use client";
 
 import { useState, useEffect } from 'react';
 import { Accordion, AccordionItem } from "@nextui-org/accordion";
 import { CheckboxGroup, Checkbox } from "@nextui-org/checkbox";
+import { ClipLoader } from 'react-spinners'; // Import the loading spinner
 
 function ToolCard({ imgSrc, title, description, pricing }) {
   return (
@@ -22,18 +23,20 @@ function ToolCard({ imgSrc, title, description, pricing }) {
 export default function Home() {
   const [toolData, setToolData] = useState([]);
   const [selectedFilter, setSelectedFilter] = useState(null); // State for selected filter
+  const [loading, setLoading] = useState(true); // State for loading
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('../api/getAItools', {
-        });
+        const response = await fetch('../api/getAItools');
         const data = await response.json();
         console.log("data:")
         console.log(data)
         setToolData(data);
       } catch (error) {
         console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false); // Set loading to false once data is fetched
       }
     };
 
@@ -45,11 +48,8 @@ export default function Home() {
   };
 
   const filteredTools = selectedFilter
-  ? toolData.filter((tool) => tool.Free_version === (selectedFilter === "free")) || toolData.filter((tool) => tool.Paid_version === (selectedFilter === "paid"))
-  : toolData; // Filter data or use all data if no filter is selected
-
-  const defaultContent =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+    ? toolData.filter((tool) => tool.Free_version === (selectedFilter === "free")) || toolData.filter((tool) => tool.Paid_version === (selectedFilter === "paid"))
+    : toolData; // Filter data or use all data if no filter is selected
 
   return (
     <div className="flex flex-col min-h-screen bg-blue-200">
@@ -59,51 +59,51 @@ export default function Home() {
         <form className="flex justify-center items-start px-3.5 py-4 mt-16 max-w-full text-2xl font-extralight text-black bg-white rounded-xl border border-black border-solid shadow-sm w-[750px] max-md:pr-5 max-md:mt-10">
           <label className="sr-only" htmlFor="toolInput">Enter a tool name</label>
           <input className="w-full bg-transparent border-none outline-none" type="text" id="toolInput" placeholder="Enter a tool name...." aria-label="Enter a tool name...." />
-          </form>
+        </form>
       </header>
 
       {/* Body */}
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 gap-4 relative bg-blue-200">
 
         {/* Filters section */}
-        <div className="md:col-span-1 p-14 ">
+        <div className="md:col-span-1 p-14">
           {/* Filters */}
-          <div className="self-start pr-10 ">
-            <section className=" ">
-              <h2 className="text-2xl font-bold mb-4 ml-2 text-black ">Filters</h2>
+          <div className="self-start pr-10">
+            <section>
+              <h2 className="text-2xl font-bold mb-4 ml-2 text-black">Filters</h2>
               <Accordion selectionMode="multiple" defaultExpandedKeys={["1", "2", "3"]}>
-                <AccordionItem   key="1" className='text-black font-semibold' aria-label="Category" title="Category">
-                  <CheckboxGroup  className=''>
-                    <Checkbox color="primary" className='font-normal'  value="buenos-aires">AI Chatbots</Checkbox>
-                    <Checkbox className='font-normal'  value="sydney">Image</Checkbox>
-                    <Checkbox className='font-normal'  value="london">Automation</Checkbox>
-                    <Checkbox className='font-normal'  value="tokyoo">Writing</Checkbox>
-                    <Checkbox className='font-normal'  value="tokyooo">Video</Checkbox>
+                <AccordionItem key="1" className='text-black font-semibold' aria-label="Category" title="Category">
+                  <CheckboxGroup>
+                    <Checkbox color="primary" className='font-normal' value="buenos-aires">AI Chatbots</Checkbox>
+                    <Checkbox className='font-normal' value="sydney">Image</Checkbox>
+                    <Checkbox className='font-normal' value="london">Automation</Checkbox>
+                    <Checkbox className='font-normal' value="tokyoo">Writing</Checkbox>
+                    <Checkbox className='font-normal' value="tokyooo">Video</Checkbox>
                   </CheckboxGroup>
                 </AccordionItem>
                 <AccordionItem key="2" className='text-black font-semibold' aria-label="Industry" title="Industry">
                   <CheckboxGroup>
-                    <Checkbox className='font-normal'  value="buenos-aires">Productivity</Checkbox>
-                    <Checkbox className='font-normal'  value="sydney">Marketing</Checkbox>
-                    <Checkbox className='font-normal'  value="san-francisco">Entertainment</Checkbox>
-                    <Checkbox className='font-normal'  value="london">Social Media</Checkbox>
-                    <Checkbox className='font-normal'  value="tokyo">Business</Checkbox>
-                    <Checkbox className='font-normal'  value="tokyo2">Business</Checkbox>
-                    <Checkbox className='font-normal'  value="tokyo3">Eductaion</Checkbox>
+                    <Checkbox className='font-normal' value="buenos-aires">Productivity</Checkbox>
+                    <Checkbox className='font-normal' value="sydney">Marketing</Checkbox>
+                    <Checkbox className='font-normal' value="san-francisco">Entertainment</Checkbox>
+                    <Checkbox className='font-normal' value="london">Social Media</Checkbox>
+                    <Checkbox className='font-normal' value="tokyo">Business</Checkbox>
+                    <Checkbox className='font-normal' value="tokyo2">Business</Checkbox>
+                    <Checkbox className='font-normal' value="tokyo3">Education</Checkbox>
                   </CheckboxGroup>
                 </AccordionItem>
                 <AccordionItem key="3" className='text-black font-semibold' aria-label="Price" title="Price">
-                      <div>
-                        <div className="flex items-center">
-                          <input type="radio" id="free" name="priceFilter" value="free" onChange={(e) => handleCheckboxChange(e.target.value)} className="w-5 h-5" />
-                          <label className='font-normal ml-2' for="free">Free</label>
-                        </div>
-                        <div className="flex items-center mt-2">
-                          <input type="radio" id="paid" name="priceFilter" value="paid" onChange={(e) => handleCheckboxChange(e.target.value)} className="w-5 h-5" />
-                          <label className='font-normal ml-2' for="paid">Paid</label>
-                        </div>
-                      </div>
-              </AccordionItem>
+                  <div>
+                    <div className="flex items-center">
+                      <input type="radio" id="free" name="priceFilter" value="free" onChange={(e) => handleCheckboxChange(e.target.value)} className="w-5 h-5" />
+                      <label className='font-normal ml-2' htmlFor="free">Free</label>
+                    </div>
+                    <div className="flex items-center mt-2">
+                      <input type="radio" id="paid" name="priceFilter" value="paid" onChange={(e) => handleCheckboxChange(e.target.value)} className="w-5 h-5" />
+                      <label className='font-normal ml-2' htmlFor="paid">Paid</label>
+                    </div>
+                  </div>
+                </AccordionItem>
               </Accordion>
             </section>
           </div>
@@ -114,19 +114,24 @@ export default function Home() {
           {/* Tool Cards */}
           <section className="self-stretch px-0.5 mt-10 max-w-[1040px] max-md:max-w-full text-black">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-            {filteredTools.map((tool, index) => (
-            <ToolCard
-              key={index}
-              imgSrc="https://cdn.builder.io/api/v1/image/assets/TEMP/5131dd1527b6bfd44de733eb18eee4b5a926586836aee4060a1661450a46f233?apiKey=062b4d44d883462aa75330f48dcf750c&"
-              title={tool.tool_name}
-              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-              pricing={tool.Free_version ? "Free" : (tool.Paid_version ? "Paid" : null)}
-            />
-          ))}
+              {loading ? (
+                <div className="flex justify-center items-center w-full col-span-1 md:col-span-2 lg:col-span-3">
+                  <ClipLoader size={50} color={"#123abc"} loading={loading} />
+                </div>
+              ) : (
+                filteredTools.map((tool, index) => (
+                  <ToolCard
+                    key={index}
+                    imgSrc="https://cdn.builder.io/api/v1/image/assets/TEMP/5131dd1527b6bfd44de733eb18eee4b5a926586836aee4060a1661450a46f233?apiKey=062b4d44d883462aa75330f48dcf750c&"
+                    title={tool.tool_name}
+                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                    pricing={tool.Free_version ? "Free" : (tool.Paid_version ? "Paid" : null)}
+                  />
+                ))
+              )}
             </div>
           </section>
         </div>
-
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,11 +14,13 @@ const Navbar = () => {
     return (
         <div className="fixed top-0 left-0 w-full lg:p-16 lg:pt-0 p-4 pt-0 z-50 ">
             <nav className="p-4 rounded-lg shadow-lg flex justify-between items-center bg-gray-800">
+                <a href="/">
                 <img
                     src="scaleup-logo.png"
                     alt="Logo"
                     className="h-10"
                 />
+                </a>
                 <div className="block lg:hidden">
                     <button
                         onClick={toggleDropdown}
@@ -30,10 +32,10 @@ const Navbar = () => {
                     </button>
                 </div>
                 <ul className="hidden lg:flex justify-around space-x-8">
-                    <li><a href="#home" className="text-white hover:text-gray-300">Home</a></li>
+                    <li><a href="/" className="text-white hover:text-gray-300">Home</a></li>
                     <li><a href="#ai-applications" className="text-white hover:text-gray-300">AI Applications</a></li>
                     <li><a href="#gpt-tools" className="text-white hover:text-gray-300">GPT Tools</a></li>
-                    <li><a href="#ai-trends" className="text-white hover:text-gray-300">AI Trends</a></li>
+                    <li><a href="trends" className="text-white hover:text-gray-300">AI Trends</a></li>
                     <li><a href="#events" className="text-white hover:text-gray-300">Events</a></li>
                     <li><a href="#contacts" className="text-white hover:text-gray-300">Contacts</a></li>
                 </ul>
@@ -50,10 +52,10 @@ const Navbar = () => {
             >
                 <div className="lg:hidden mt-2 bg-gray-800 rounded-lg shadow-lg">
                     <ul className="flex flex-col items-start space-y-2 p-4">
-                        <li><a href="#home" className="text-white hover:text-gray-300">Home</a></li>
+                        <li><a href="/" className="text-white hover:text-gray-300">Home</a></li>
                         <li><a href="#ai-applications" className="text-white hover:text-gray-300">AI Applications</a></li>
                         <li><a href="#gpt-tools" className="text-white hover:text-gray-300">GPT Tools</a></li>
-                        <li><a href="#ai-trends" className="text-white hover:text-gray-300">AI Trends</a></li>
+                        <li><a href="trends" className="text-white hover:text-gray-300">AI Trends</a></li>
                         <li><a href="#events" className="text-white hover:text-gray-300">Events</a></li>
                         <li><a href="#contacts" className="text-white hover:text-gray-300">Contacts</a></li>
                         <li><a href="#login" className="text-white hover:text-gray-300">Login</a></li>
